d209f17c5d9afcb94441bc3e58b8dba695996213
Brett Henderson <BrettsVoid@gmail.com>
2024-12-03 22:04:49 +0000

    feat: add textures

--- src/particle_effects.rs
+++ src/particle_effects.rs
@@ -1,5 +1,4 @@
-use macroquad::prelude::*;
-use macroquad_particles::{self as particles, ColorCurve, Emitter, EmitterConfig};
+use macroquad_particles::{self as particles, AtlasConfig};
 
 pub fn explosion() -> particles::EmitterConfig {
     particles::EmitterConfig {
@@ -10,15 +9,11 @@ pub fn explosion() -> particles::EmitterConfig {
         lifetime_randomness: 0.3,
         explosiveness: 0.65,
         initial_direction_spread: 2.0 * std::f32::consts::PI,
-        initial_velocity: 300.0,
+        initial_velocity: 400.0,
         initial_velocity_randomness: 0.8,
-        size: 3.0,
+        size: 16.0,
         size_randomness: 0.3,
-        colors_curve: ColorCurve {
-            start: RED,
-            mid: ORANGE,
-            end: RED,
-        },
+        atlas: Some(AtlasConfig::new(5, 1, 0..)),
         ..Default::default()
     }
 }
